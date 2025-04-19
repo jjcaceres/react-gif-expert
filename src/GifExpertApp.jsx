8bbed93c13cd68805bffde327cc0be19dcd010e7
Jose Caceres <jose.caceres.mamani@gmail.com>
2025-04-18 23:06:47 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components'
+
+
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch'])
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        //categories.push(newCategory);
+        setCategories([newCategory, ...categories]);
+    }
+    return (
+        <>
+
+            <h1>GifExpertApp</h1>
+            <AddCategory
+                onNewCategory={(value) => onAddCategory(value)}
+                currentCategories={categories}
+            />
+
+            {
+                categories.map((category, i) => (
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+
+
+        </>
+
+    )
+}
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+
+
+
+export const GifGrid = ({ category }) => {
+
+    const { images, isLoading } = useFetchGifs(category);
+
+ 
+    /*  const [images, setImages] = useState([])
+     const getImages = async () => {
+         const newImages = await getGifs(category);
+         console.log(newImages)
+         setImages(newImages);
+     }
+     useEffect(() => {
+         getImages();
+         
+     }, []) */
+
+    /* getGifs(category)
+                .then(newImages => setImages(newImages)); */
+
+    return (
+        <>
+            <h3>{category}</h3>
+            {
+               /*  isLoading
+                ?(<h2 className="">Cargando ...</h2>)
+                : null */
+                isLoading && (<h2 className="">Cargando ...</h2>)
+            }
+            
+
+            <div className="card-grid">
+                {
+                    images.map((image) => (
+                        <GifItem key={image.id} {...image} />
+                    ))
+                }
+
+
+
+            </div>
+
+
+
+
+        </>
+    )
+}
